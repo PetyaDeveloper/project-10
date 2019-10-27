f81dd3714a337916a14a0d51930a662c9c36a10b
PetyaDeveloper <pskpetya@gmail.com>
2019-10-27 15:24:08 +0400

    fixing the [object object] bug for the street name and number

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function displayInfo(data){
         </span>
         <div class="personExtraData" style="display:none">
           <p>${item.cell}</p>
-          <p class="address">${item.location.street}, ${item.location.state}, ${item.location.postcode}</p>
+          <p class="address">${item.location.street.name} ${item.location.street.number}, ${item.location.state}, ${item.location.postcode}</p>
           <p>Birthday: ${date}/${month}/${year}</p>
           
           <span class = "arrows">
@@ -24,6 +24,7 @@ function displayInfo(data){
             <button id="next">&#10095;</button>
           </span>
         </div></li>`;
+        console.log(`${item.location.street.name}`)
     });
     personList += '<ul><table>';
     $('.js').html(personList);
